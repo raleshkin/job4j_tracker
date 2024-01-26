8c7fe7c1099b6592eb5984f8394a5e8ae9eb4dd8
raleshkin <free-teen2009@yandex.ru>
2024-01-26 16:38:00 +0700

    6. Правила Checkstyle [#504981 #476341]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -3,24 +3,25 @@ package ru.job4j.checkstyle;
 public class Broken {
 
     private int sizeOfEmpty = 10;
+
     private String surname;
 
     private static final String NEW_VALUE = "";
 
+    private String name;
+
     public void echo() {
+    }
+
+    public Broken() {
+    }
+
+    public void method() {
     }
 
     public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
-    }
-
-    public void method() {
-    }
-
-    private String name;
-
-    public Broken() {
     }
 }
