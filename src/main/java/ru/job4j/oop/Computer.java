e481bfb8e206b8f2f88fb4d8074950b2427d82e5
raleshkin <free-teen2009@yandex.ru>
2024-02-01 09:52:19 +0700

    1.7. Конструктор [#173889 #478204]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -5,15 +5,16 @@ public class Computer {
     private boolean multiMonitor;
     private int ssd;
     private String cpu;
+
     public Computer() {
     }
-
 
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
     }
+
     public static void main(String[] args) {
         Computer computer = new Computer();
         computer.printInfo();
