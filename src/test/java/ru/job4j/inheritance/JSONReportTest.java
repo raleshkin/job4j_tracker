5ffe1386e2ccd46c222950bdbc3ff284d5faa6a2
raleshkin <free-teen2009@yandex.ru>
2024-02-19 19:06:37 +0700

    4. Переопределение метода [#174890]

---
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JSONReportTest {
+
+    @Test
+    void generate() {
+    }
+}
