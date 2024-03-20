199fd441b6f7f98b0d05fb679fb0af2dd479f238
raleshkin <free-teen2009@yandex.ru>
2024-03-20 11:30:42 +0700

    6. Метод замены заявки. Tracker.replace [#211748 #486786]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -50,7 +50,8 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            items[index].setName(item.getName());
+            item.setId(items[index].getId());
+            items[index] = item;
         }
         return result;
     }
