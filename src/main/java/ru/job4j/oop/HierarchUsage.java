718a67ecdd3568ff9e670dbebeab7b1af3ce247f
raleshkin <free-teen2009@yandex.ru>
2024-03-28 17:37:17 +0700

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/HierarchUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class HierarchUsage {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Transport transport = car;
+        Object object = car;
+        Object objectCar = new Car();
+        Car carFromObject = (Car) objectCar;
+
+        Object bicycle = new Bicycle();
+        Car carBicycle = (Car) bicycle;
+    }
+}
