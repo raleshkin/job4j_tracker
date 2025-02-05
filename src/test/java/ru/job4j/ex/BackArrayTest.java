6a39c69af89d9e96528b340b98a0c2b629ec866e
raleshkin <free-teen2009@yandex.ru>
2025-02-06 00:12:45 +0700

    0. Что такое исключение. [#3821]

--- src/test/java/ru/job4j/ex/BackArrayTest.java
+++ src/test/java/ru/job4j/ex/BackArrayTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.ex;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BackArrayTest {
@@ -8,15 +9,15 @@ class BackArrayTest {
     void whenInputArrayHasEvenAmountElements() {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
         String[] result = new BackArray().turn(names);
-        String[] excepted = {"Vasya", "Nik", "Ivan", "Petr"};
-        assertThat(result).isEqualTo(excepted);
+        String[] expected = {"Vasya", "Nik", "Ivan", "Petr"};
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenInputArrayHasNotEvenAmountElements() {
         String[] names = {"Petr", "Ivan", "Andrey", "Nik", "Vasya"};
         String[] result = new BackArray().turn(names);
-        String[] excepted = {"Vasya", "Nik", "Andrey", "Ivan", "Petr"};
-        assertThat(result).isEqualTo(excepted);
+        String[] expected = {"Vasya", "Nik", "Andrey", "Ivan", "Petr"};
+        assertThat(result).isEqualTo(expected);
     }
 }
